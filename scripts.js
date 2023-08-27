7c99a2bb735f370db853686369e89c22cc7e2a22
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-08-27 10:55:45 -0300

    Otimização de código

--- scripts.js
+++ scripts.js
@@ -1,33 +1,37 @@
+const input = document.getElementById("input-busca")
+
 function botaoDeBusca(){
-   const input = document.getElementById("input-busca").value
+   const inputValue = input.value;
    movimentoInput()
 }
 
 function fecharInput(){
-   document.getElementById("input-busca").style.visibility = 'hidden';
-   document.getElementById("input-busca").style.width = '40px';
-   document.getElementById("input-busca").style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
-   document.getElementById("input-busca").style.transition = 'all 0.5s ease-in-out 0s';
-   document.getElementById('input-busca').value = ""; /*limpar o texto escrito pelo usuario no Entry*/
+   input.style.width = '40px';
+   input.style.visibility = 'hidden';
+   input.style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
+   input.style.transition = 'all 0.5s ease-in-out 0s';
+   input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
 }
 
 function abrirInput(){
-   document.getElementById("input-busca").style.visibility = 'visible';
-   document.getElementById("input-busca").style.width = '300px';
-   document.getElementById("input-busca").style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
-   document.getElementById("input-busca").style.transition = 'all 0.5s ease-in-out 0s';
-   document.getElementById('input-busca').value = ""; /*limpar o texto escrito pelo usuario no Entry*/
+   input.style.visibility = 'visible';
+   input.style.width = '300px';
+   input.style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
+   input.style.transition = 'all 0.5s ease-in-out 0s';
+   input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
 }
 
 function movimentoInput(){
    const visibility = document.getElementById('input-busca').style.visibility
-   if (visibility === 'hidden'){
-      abrirInput()
-   }
-   else{
-      fecharInput()
-   }
+   visibility === 'hidden' ? abrirInput() : fecharInput(); /*if/else de uma linha só*/
 }
+
+input.addEventListener('keyup', function (event){
+   if (event.keyCode === 13){
+      const valorInput = input.value;
+      movimentoInput(valorInput)
+   }
+})
 
 document.addEventListener("DOMContentLoaded", () => {/*Arrow function, pesquisar sobre isso*/
    fecharInput()
