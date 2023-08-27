55a1f97aba93458d627e742599bd282a8729e4f8
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-08-27 17:55:31 -0300

    api com openwather concluida

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 const input = document.getElementById("input-busca")
+const apiKey = '';
+
+function movimentoInput(inputValue){
+   const visibility = document.getElementById('input-busca').style.visibility
+   inputValue && procurarCidade(inputValue);
+   visibility === 'hidden' ? abrirInput() : fecharInput(); /*if/else de uma linha só*/
+}
 
 function botaoDeBusca(){
    const inputValue = input.value;
-   movimentoInput()
+   movimentoInput(inputValue)
 }
 
 function fecharInput(){
@@ -21,11 +28,6 @@ function abrirInput(){
    input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
 }
 
-function movimentoInput(){
-   const visibility = document.getElementById('input-busca').style.visibility
-   visibility === 'hidden' ? abrirInput() : fecharInput(); /*if/else de uma linha só*/
-}
-
 input.addEventListener('keyup', function (event){
    if (event.keyCode === 13){
       const valorInput = input.value;
@@ -36,3 +38,29 @@ input.addEventListener('keyup', function (event){
 document.addEventListener("DOMContentLoaded", () => {/*Arrow function, pesquisar sobre isso*/
    fecharInput()
 })
+
+async function procurarCidade(inputValue){
+   try{
+      const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric&lang=pt_br`)
+      if (dados.status === 200){
+         const resultado = await dados.json();
+         mostrarClimaNaTela(resultado);
+         console.log(resultado, '<<');
+      }
+      else{
+         throw new Error
+      }
+   }catch{
+      alert("A pesquisa deu errado")
+   }
+}
+
+function mostrarClimaNaTela(resultado){
+   document.querySelector('.nome-cidade').innerHTML = `${resultado.name}`;
+   document.querySelector('.temperatura').innerHTML = `${resultado.main.temp.toFixed(0)}°C`; /*a função tofixed é para determinar quantos numeros apos a virgulaqueremos, como a gnt nao quer nenhum numero apos a virgula a gente deixa o tofixed como 0*/
+   document.querySelector('.maxTemperatura').innerHTML = `Máx: ${resultado.main.temp_max.toFixed(0)}°C`;
+   document.querySelector('.minTemperatura').innerHTML = `Min: ${resultado.main.temp_min.toFixed(0)}°C`;
+   document.querySelector('.icone-tempo').src = `./images/${resultado.weather[0].icon}.png`
+}
+
+
